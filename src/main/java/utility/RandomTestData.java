2c8b5cbdd3eccd998aa94168d23ed8f6ff02a583
sachin.jadhav222 <sachin.jadhav222@yahoo.com>
2021-03-30 01:30:16 +0100

    JSON payload

--- src/main/java/utility/RandomTestData.java
+++ src/main/java/utility/RandomTestData.java
@@ -2,6 +2,9 @@ package utility;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.json.simple.JSONObject;
+
+import org.junit.Test;
 
 public class RandomTestData {
 
@@ -22,14 +25,18 @@ public class RandomTestData {
         return (generatedString);
     }
 
-    public static String geEmail(){
+    public static String getEmail(){
         String generatedString= RandomStringUtils.randomAlphabetic(3);
         return (generatedString+"gmail.com");
     }
 
-    public static JSONPObject getPayload(){
+    public static String  getJSONPayload(){
+        JSONObject json=new JSONObject();
+        json.put("name",getFirstName());
+        json.put("username",getUserName());
+        json.put("email",getEmail());
 
-        return null;
+        return json.toString();
     }
 
 }
